88d938a5f9d10fec490e8f37a152d1f442b85d09
Liran Parti <liranpar@gmail.com>
2022-03-22 21:46:33 +0200

    added header

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,13 +1,17 @@
 <template>
-  
+  <header class="main-header">
+    <span class="logo">restInn</span>
+    <div>
+      <router-link to="/">Lidor</router-link>
+      <router-link to="/">Ya</router-link>
+      <router-link to="/">Maniac</router-link>
+      <router-link to="/">About</router-link>
+    </div>
+  </header>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
