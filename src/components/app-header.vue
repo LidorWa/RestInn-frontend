92788e9f235fd873bb3c2c1b8bea2ffd018d4846
Lidor Waldman <lidorvl@gmail.com>
2022-03-22 22:32:20 +0200

    added basic nav btwn main pages

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,5 +1,10 @@
 <template>
-  
+    <nav class="main-header">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/stay">Explore</router-link>
+
+  </nav>
 </template>
 
 <script>
