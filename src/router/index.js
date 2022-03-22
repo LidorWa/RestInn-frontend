38496a95492981ee983a5872c4727fc67e715224
Tal Itay <talitay7@gmail.com>
2022-03-23 01:27:33 +0200

    collaborate commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-import homePage from '../views/home-page.vue';
-import aboutPage from '../views/about-page.vue';
-import loginSignup from '../views/login-signup.vue';
-import stayDetails from '../views/stay-details.vue';
-import stayEdit from '../views/stay-edit.vue';
-import userDetails from '../views/user-details.vue';
-import dashboard from '../views/dashboard.vue';
-import stayApp from '../views/stay-app.vue';
-import backoffice from '../views/backoffice.vue';
-
+import { createRouter, createWebHashHistory } from 'vue-router'
+import homePage from '../views/home-page.vue'
+import aboutPage from '../views/about-page.vue'
+import loginSignup from '../views/login-signup.vue'
+import stayDetails from '../views/stay-details.vue'
+import stayEdit from '../views/stay-edit.vue'
+import userDetails from '../views/user-details.vue'
+import dashboard from '../views/dashboard.vue'
+import stayApp from '../views/stay-app.vue'
+// import backoffice from '../views/backoffice.vue';
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -16,26 +15,24 @@ const router = createRouter({
     {
       name: 'home-page',
       path: '/',
-      component: homePage
+      component: homePage,
     },
     {
       name: 'about-page',
       path: '/about',
-      component: aboutPage
-      
+      component: aboutPage,
     },
     {
       name: 'stay-app',
       path: '/stay',
-      component: stayApp
-      
+      component: stayApp,
     },
     {
-    name: 'stay-details',
-    path: '/stay/:stayId',
-    component: stayDetails 
-  },  
-  ]
+      name: 'stay-details',
+      path: '/stay/:stayId',
+      component: stayDetails,
+    },
+  ],
 })
 
 export default router
