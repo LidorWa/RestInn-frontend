bf044b04d8c4b0596ba13f4fcfedc170922ace60
Liran Parti <liranpar@gmail.com>
2022-03-22 22:35:28 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import appHeader from "./components/app-header.vue";
-</script>
-
 <template>
   <section>
     <app-header />
     <RouterView />
   </section>
 </template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import appHeader from "./components/app-header.vue";
+</script>
