729ca072b0657ba1e42e63f72bef9be90d33565d
Liran Parti <liranpar@gmail.com>
2022-03-22 21:37:38 +0200

    applied json conversion

--- src/views/stay-app.vue
+++ src/views/stay-app.vue
@@ -1,13 +1,38 @@
 <template>
-  
+  <section></section>
 </template>
 
 <script>
+import { stayService } from "../services/stay-service.js";
+
 export default {
+  name: "stay-app",
+  data() {
+    return {
+      stays: stayService.query(),
+    };
+  },
+  computed: {
+    // getStays() {
+    //   return this.$store.getters.getStays;
+    // },
+    // staysForDisplay() {
+    //   return this.$store.getters.staysForDisplay;
+    // },
+  },
+  created() {
+    console.log(this.stays);
+  },
+  methods: {
+    setFilter(filterBy) {
+      this.$store.dispatch({ type: "filter", filterBy });
+    },
+  },
 
-}
+  components: {
+    // stayList,
+    // stayFilter,
+    // addStay,
+  },
+};
 </script>
-
-<style>
-
-</style>
