258553372458916c0d5363ce9daa336edf3e73ad
Liran Parti <liranpar@gmail.com>
2022-03-23 11:01:46 +0200

    /

--- src/components/stay-preview.vue
+++ src/components/stay-preview.vue
@@ -1,12 +1,22 @@
 <template>
   <li class="card">
-    <div class="card-img">
+    <div>
       <div class="carusele">
-        <img :src="getImgUrl(stay.imgUrls[0])" />
+        <!-- <img :src="getImgUrl" alt="" /> -->
+        <!-- arrow="always" -->
+        <el-carousel :autoplay="false">
+          <el-carousel-item v-for="item in stay.imgUrls" :key="item">
+            <img class="card-img" :src="getImgUrl(item)" alt="" />
+          </el-carousel-item>
+        </el-carousel>
       </div>
       <div class="like"></div>
     </div>
-    <span class="material-icons-outlined star">star</span>
+    <p class="card-review-cont">
+      <span class="material-icons-outlined star">star</span
+      ><span class="card-rate">{{ getStayRate(stay) }}</span>
+      <span class="reviews-count">({{ getReviewCount(stay) }})</span>
+    </p>
     <span>{{ stay.name }}</span>
     <router-link :to="'/stay/' + stay._id">Details</router-link>
     <!-- <img v-if="stay.img" :src="stay.img" class="stay-img" /> -->
@@ -26,9 +36,37 @@ export default {
     // carPreview,
   },
   methods: {
+    getStayRate(stay) {
+      return (stay.reviewScores.rating / 20).toFixed(2);
+    },
+    getReviewCount(stay) {
+      return stay.reviews.length + " Reviews";
+    },
     getImgUrl(file) {
-      return `../assets/images/${file}`;
+      const imgUrl = new URL(`../assets/images/${file}`, import.meta.url);
+
+      return imgUrl;
     },
   },
+  computed: {},
 };
 </script>
+
+<style scoped>
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 18px;
+  opacity: 0.75;
+  line-height: 300px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
