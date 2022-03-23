cbffb3fe6821c781e0f0c54ee765c7cd3397ac8c
Tal Itay <talitay7@gmail.com>
2022-03-23 21:53:23 +0200

    progress with header

---
+++ src/components/main-search.vue
@@ -0,0 +1,59 @@
+<template>
+  <section>
+    <form @submit.prevent="" class="search-form-container flex">
+      <div class="input-container location-input-container">
+        <label for="location-input" class="location-input">Location</label>
+        <input type="text" id="location-input" name="location-input" placeholder="Where are you going?" autocomplete="off" spellcheck="false" />
+      </div>
+
+      <div class="input-container date-picker-container">
+        <div class="block">
+          <el-date-picker v-model="value1" type="datetimerange" start-placeholder="Start Date" end-placeholder="End Date" :default-time="defaultTime1" />
+        </div>
+      </div>
+      <div class="input-container guestsInput-container">
+        <label for="guestsInput" class="">Geusts</label>
+        <input type="number" id="guestsInput" name="guestsInput" placeholder="How Many?" />
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+import { ref } from 'vue'
+// import { fa } from 'element-plus/lib/locale'
+
+export default {
+  name: 'main-search',
+  data() {
+    return {
+      value1: ref(''),
+      headerObserver: null,
+      stickyNav: false,
+    }
+  },
+  components: {},
+  mounted() {
+    // this.headerObserver = new IntersectionObserver(this.onHeaderObserved, {
+    //   rootMargin: '-100px 0px 0px',
+    // })
+    // this.headerObserver.observe(this.$ref.header)
+  },
+  created() {},
+  methods: {
+    // onHeaderObserved(entries) {
+    //   entries.forEach((entry) => {
+    //     this.stickyNav = entry.isIntersecting ? false : true
+    //   })
+    // },
+  },
+  computed: {
+    defaultTime1() {
+      ;[new Date(2000, 1, 1, 12, 0, 0)] // '12:00:00'
+    },
+  },
+  unmounted() {},
+}
+</script>
+
+<style></style>
