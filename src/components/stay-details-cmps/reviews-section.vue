ab9b9c9149a4ad7e015ccf71e8358cbab9497b8f
Lidor Waldman <lidorvl@gmail.com>
2022-03-24 02:14:20 +0200

    new child cmp reviews-section for stay-details

---
+++ src/components/stay-details-cmps/reviews-section.vue
@@ -0,0 +1,26 @@
+<template>
+            <section class="reviews-section">
+            <div class="reviews-and-rating">
+              <h2>
+                <img src="../../assets/svgs/star-rating.svg">
+                <span>{{ stay.reviewScores.rating / 20 }} · {{stay.reviews.length}}</span>
+              </h2>
+            </div>
+            
+          </section>
+</template>
+
+<script>
+export default {
+    props:{
+        stay:{
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
