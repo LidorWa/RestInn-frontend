dd3623c4516c080d303d23ecadd5b316673aecb2
Lidor Waldman <lidorvl@gmail.com>
2022-03-24 02:13:23 +0200

    added hard-coded amenities list

---
+++ src/components/stay-details-cmps/amenities-list.vue
@@ -0,0 +1,57 @@
+<template>
+            <section class="main-amenities-list">
+            <div class="amenities-list-title">
+              <h1>What this place offers</h1>
+            </div>
+            <div class="amenity">
+              <img src="../../assets/svgs/air-conditioning.svg">
+              <span>Air-conditioning</span>
+            </div>
+                        <div class="amenity">
+              <img src="../../assets/svgs/courtyard-view.svg">
+              <span>Courtyard view</span>
+            </div>
+                        <div class="amenity">
+              <img src="../../assets/svgs/dedicated-workspace.svg">
+              <span>Dedicated workspace</span>
+            </div>
+                        <div class="amenity">
+              <img src="../../assets/svgs/elevator.svg">
+              <span>Elevator</span>
+            </div>
+                        <div class="amenity">
+              <img src="../../assets/svgs/free-parking.svg">
+              <span>Free parking</span>
+            </div>
+                        <div class="amenity">
+              <img src="../../assets/svgs/kitchen.svg">
+              <span>Kitchen</span>
+            </div>
+                        <div class="amenity">
+              <img src="../../assets/svgs/long-term.svg">
+              <span>Long-term stay allowed</span>
+            </div>
+                        <div class="amenity">
+              <img src="../../assets/svgs/pets-allowed.svg">
+              <span>Pets allowed</span>
+            </div>
+                                   <div class="amenity">
+              <img src="../../assets/svgs/refrigerator.svg">
+              <span>Refrigerator</span>
+            </div>
+                        <div class="amenity">
+              <img src="../../assets/svgs/wifi.svg">
+              <span>Wifi</span>
+            </div>
+          </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
