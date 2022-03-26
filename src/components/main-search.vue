ce722f2a0849baa3ee7fe86f06087eeaa1e8ed21
Liran Parti <liranpar@gmail.com>
2022-03-27 00:08:51 +0300

    team sync

--- src/components/main-search.vue
+++ src/components/main-search.vue
@@ -1,43 +1,71 @@
 <template>
   <section>
-    <form @submit.prevent="" class="main-search-container flex align-center btn">
+    <form
+      @submit.prevent=""
+      class="main-search-container flex align-center btn"
+    >
       <!-- location -->
       <div class="header-input location-input flex flex-column">
-        <label for=" location-input" class="label location-input">Location</label>
-        <input type="text" v-model="location" id="location-input" name="location-input" :placeholder="getMainSearchText" autocomplete="off" spellcheck="false" />
+        <label for=" location-input" class="label">Location</label>
+        <input
+          type="text"
+          v-model="location"
+          id="location-input"
+          name="location-input"
+          :placeholder="getMainSearchText"
+          autocomplete="off"
+          spellcheck="false"
+        />
       </div>
 
       <span class="search-space"></span>
       <!-- date picker -->
       <!-- check in -->
       <div class="date-picker-container flex">
-        <div class="header-input check-in-top flex flex-column">
+        <div class="header-input date-input check-in-top flex flex-column">
           <div class="label check-in-top">Check in</div>
-          <div class="header-input">{{ formatedDate(0) }}</div>
+          <div class="add-dates">{{ formatedDate(0) }}</div>
         </div>
 
         <span class="search-space"></span>
         <!-- check out -->
-        <div class="header-input check-out-top flex flex-column">
+        <div class="header-input date-input check-out-top flex flex-column">
           <div class="label check-out-top">Check out</div>
-          <div>{{ formatedDate(1) }}</div>
+          <div class="add-dates">{{ formatedDate(1) }}</div>
         </div>
         <!-- element picker -->
         <div class="block date-picker">
           <!-- returns timestamp (Value format "x") -->
-          <el-date-picker v-model="dates" type="daterange" range-separator="To" start-placeholder="Start date" end-placeholder="End date" value-format="x" />
+          <el-date-picker
+            v-model="dates"
+            type="daterange"
+            range-separator="To"
+            start-placeholder="Start date"
+            end-placeholder="End date"
+            value-format="x"
+          />
         </div>
       </div>
       <span class="search-space"></span>
 
       <!-- geusts -->
       <div class="header-input guestsInput flex">
-        <label for="guestsInput" class="label">Guests</label>
-
-        <div class="label guests-input" @click="isSelectingGuests = !isSelectingGuests" placeholder="Add guests">
-          {{ getGuestsNumber }}
+        <div class="guests-input-container flex flex-column">
+          <label for="guestsInput" class="label">Guests</label>
+          <div
+            class="guests-input"
+            @click="isSelectingGuests = !isSelectingGuests"
+            placeholder="Add guests"
+          >
+            {{ getGuestsNumber }}
+          </div>
         </div>
-        <select-guests-modal v-if="isSelectingGuests" :guests="guests" @closeGuestsModal="closeGuestsModal" @onSelectGuests="onSelectGuests" />
+        <select-guests-modal
+          v-if="isSelectingGuests"
+          :guests="guests"
+          @closeGuestsModal="closeGuestsModal"
+          @onSelectGuests="onSelectGuests"
+        />
         <button class="search-icon" @click="onSearch">
           <img src="../assets/svgs/search.svg" alt="search Icon" />
         </button>
@@ -49,13 +77,13 @@
 </template>
 
 <script>
-import { ref } from 'vue'
-import selectGuestsModal from '../components/select-guests-modal.vue'
+import { ref } from "vue";
+import selectGuestsModal from "../components/select-guests-modal.vue";
 
 // import { fa } from 'element-plus/lib/locale'
 
 export default {
-  name: 'main-search',
+  name: "main-search",
   props: {
     savedLocation: {
       type: String,
@@ -72,10 +100,10 @@ export default {
       headerObserver: null,
       stickyNav: false,
       isSelectingGuests: false,
-      location: '',
+      location: "",
       dates: null,
       guests: { adults: 0, children: 0 },
-    }
+    };
   },
   components: {
     selectGuestsModal,
@@ -87,29 +115,31 @@ export default {
     // this.headerObserver.observe(this.$ref.header)
   },
   created() {
-    this.location = this.savedLocation
-    this.dates = this.savedDates
-    this.guests = this.savedGuests
+    this.location = this.savedLocation;
+    this.dates = this.savedDates;
+    this.guests = this.savedGuests;
   },
   methods: {
     formatedDate(num) {
-      if (!this.dates) return 'Add dates'
-      const date = new Date(this.dates[num])
-      return `${date.getDate() < 10 ? '0' + date.getDate() : date.getDate()}/${date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1}/${date.getFullYear().toString().substring(2)}`
+      if (!this.dates) return "Add dates";
+      const date = new Date(this.dates[num]);
+      return `${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}/${
+        date.getMonth() < 9 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1
+      }/${date.getFullYear().toString().substring(2)}`;
     },
     closeGuestsModal() {
-      this.isSelectingGuests = false
+      this.isSelectingGuests = false;
     },
     onSelectGuests(guests) {
-      this.guests = guests
+      this.guests = guests;
     },
     onSearch() {
       // if (!this.location && !this.dates && !this.guests.adults) {
       //   this.$router.push("/stay");
       // }
-      let path = '/stay'
-      if (this.location) path += `?destination=${this.location}`
-      this.$router.push(`${path}`)
+      let path = "/stay";
+      if (this.location) path += `?destination=${this.location}`;
+      this.$router.push(`${path}`);
       // Reload???
     },
     // onHeaderObserved(entries) {
@@ -120,24 +150,24 @@ export default {
   },
   computed: {
     getMainSearchText() {
-      if (this.$route.query.destination) return this.$route.query.destination
-      return 'Where are you going?'
+      if (this.$route.query.destination) return this.$route.query.destination;
+      return "Where are you going?";
     },
     getGuestsNumber() {
-      const guests = this.guests.adults + this.guests.children
-      if (!guests) return 'Add guests'
-      if (guests === 1) return '1 guest'
-      return `${guests} guests`
+      const guests = this.guests.adults + this.guests.children;
+      if (!guests) return "Add guests";
+      if (guests === 1) return "1 guest";
+      return `${guests} guests`;
     },
     //TODO: check if needed, delete hour
     defaultTime1() {
-      ;[new Date(2000, 1, 1, 12, 0, 0)] // '12:00:00'
+      [new Date(2000, 1, 1, 12, 0, 0)]; // '12:00:00'
     },
   },
   unmounted() {
-    this.$emit('mainSearchClosed', this.location, this.dates, this.guests)
+    this.$emit("mainSearchClosed", this.location, this.dates, this.guests);
   },
-}
+};
 </script>
 
 <style></style>
