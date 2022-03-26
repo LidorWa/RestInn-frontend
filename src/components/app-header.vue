90fa8ece2be91617f09e176aa3cb74c05d143f48
Liran Parti <liranpar@gmail.com>
2022-03-26 15:05:55 +0300

    team sync

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,5 +1,8 @@
 <template>
-  <header class="main-header-container flex flex-column align-center" :class="getHeaderClass">
+  <header
+    class="main-header-container flex flex-column align-center"
+    :class="getHeaderClass"
+  >
     <!-- <header class="main-header-container flex flex-column align-center" :class="{ top: headerStatus === 'top', shrinkSearchBar: headerStatus === 'shrinkSearchBar', homepage: this.$route.path === '/', 'explore-page': this.$route.path === '/stay', 'details-page': this.$route.path === '/stay/:stayId' }"> -->
     <!-- <header class="main-header-container flex flex-column align-center" :class="headerStatus">
      -->
@@ -10,15 +13,19 @@
         <h1 class="logo-txt">RestInn</h1>
       </div>
       <!-- Mini search bar -->
-      <div v-if="checkMiniSearch" @click="toggleMiniSearch" class="search mini-search inline-flex justify-center align-center space-between">
-        <!--  -->
-        <div v-if="checkMiniSearch" @click="toggleMiniSearch" class="search mini-search inline-flex justify-center align-center space-between">
-          <div>Start your search</div>
-          <div class="search-icon">
-            <img src="../assets/svgs/search.svg" alt="search Icon" />
-          </div>
+
+      <!--  -->
+      <div
+        v-if="checkMiniSearch"
+        @click="toggleMiniSearch"
+        class="search mini-search inline-flex justify-center align-center space-between"
+      >
+        <div>Start your search</div>
+        <div class="search-icon">
+          <img src="../assets/svgs/search.svg" alt="search Icon" />
         </div>
       </div>
+
       <!--  -->
       <nav class="main-header-nav flex justify-center align-center">
         <router-link to="/stay">Explore</router-link>
@@ -61,7 +68,7 @@
         :savedGuests="getGuests"
       />
     </div>
-    <sign-up :class="{ showSignUp: isSignUp }" />
+    <!-- <sign-up :class="{ showSignUp: isSignUp }" /> -->
   </header>
 </template>
 
@@ -70,11 +77,11 @@ import mainSearch from "./main-search.vue";
 import headerUserMenu from "./header-user-menu.vue";
 import signUp from "../components/sign-up.vue";
 export default {
-  name: 'app-header',
+  name: "app-header",
   props: {
     headerStatus: {
       type: String,
-      default: 'top',
+      default: "top",
     },
     scrollY: {
       type: Number,
@@ -101,10 +108,10 @@ export default {
       // this.guests = guests;
     },
     toggleMiniSearch() {
-      this.isMiniSearchShown = !this.isMiniSearchShown
+      this.isMiniSearchShown = !this.isMiniSearchShown;
     },
     goHome() {
-      this.$router.push('/')
+      this.$router.push("/");
     },
   },
   computed: {
@@ -119,30 +126,38 @@ export default {
     },
     getHeaderClass() {
       return {
-        top: this.headerStatus === 'top',
-        shrinkSearchBar: this.headerStatus === 'shrinkSearchBar',
-        homepage: this.$route.path === '/',
-        'explore-page': this.$route.path === '/stay',
-        'details-page': this.$route.path.length > 10,
-        'main-search-open': this.isMiniSearchShown && this.scrollY > 20,
-      }
+        top: this.headerStatus === "top",
+        shrinkSearchBar: this.headerStatus === "shrinkSearchBar",
+        homepage: this.$route.path === "/",
+        "explore-page": this.$route.path === "/stay",
+        "details-page": this.$route.path.length > 10,
+        "main-search-open": this.isMiniSearchShown && this.scrollY > 20,
+      };
     },
     checkMiniSearch() {
-      return (this.headerStatus === 'shrinkSearchBar' && !this.isMiniSearchShown) || (this.$route.path.length > 10 && !this.isMiniSearchShown)
+      return (
+        (this.headerStatus === "shrinkSearchBar" && !this.isMiniSearchShown) ||
+        (this.$route.path.length > 10 && !this.isMiniSearchShown)
+      );
     },
     checkMainSearch() {
-      return (this.headerStatus !== 'shrinkSearchBar' && this.$route.path.length < 10 && (this.$route.path === '/' || this.$route.path.includes('/stay'))) || (this.isMiniSearchShown && this.headerStatus === 'shrinkSearchBar')
+      return (
+        (this.headerStatus !== "shrinkSearchBar" &&
+          this.$route.path.length < 10 &&
+          (this.$route.path === "/" || this.$route.path.includes("/stay"))) ||
+        (this.isMiniSearchShown && this.headerStatus === "shrinkSearchBar")
+      );
     },
   },
   watch: {
     headerStatus() {
-      console.log('headerStatus is:', this.headerStatus)
+      console.log("headerStatus is:", this.headerStatus);
       switch (this.headerStatus) {
-        case 'top':
-          this.isMiniSearchShown = false
-          break
-        case 'shrinkSearchBar':
-          break
+        case "top":
+          this.isMiniSearchShown = false;
+          break;
+        case "shrinkSearchBar":
+          break;
       }
     },
     scrollY() {
@@ -154,7 +169,7 @@ export default {
     headerUserMenu,
     signUp,
   },
-}
+};
 </script>
 
 <style></style>
