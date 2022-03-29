a73b9f5ccbfc41e7074c9c3cab615e6aa73fb875
Tal Itay <talitay7@gmail.com>
2022-03-29 10:09:50 +0300

    guests picker modal styling

--- src/components/select-guests-modal.vue
+++ src/components/select-guests-modal.vue
@@ -1,45 +1,25 @@
 <template>
   <section class="select-guests-modal">
     <p class="guests-modal-title">Select guests</p>
-    <div class="guests-type-conteiner">
+    <div class="guests-type-container">
       <span class="guests-type">Adults</span>
 
       <div class="guests-counter">
-        <span
-          class="material-icons guest-cout-icon"
-          @click="changeCount('adults', -1)"
-        >
-          remove
-        </span>
+        <span class="material-icons guest-count-icon" @click="changeCount('adults', -1)"> remove </span>
         <span class="guests-number">{{ guestSelected.adults }}</span>
-        <span
-          class="material-icons guest-cout-icon"
-          @click="changeCount('adults', 1)"
-        >
-          add
-        </span>
+        <span class="material-icons guest-count-icon" @click="changeCount('adults', 1)"> add </span>
       </div>
     </div>
 
-    <div class="guests-type-conteiner">
+    <div class="guests-type-container">
       <span class="guests-type">Children</span>
       <div class="guests-counter">
-        <span
-          class="material-icons guest-cout-icon"
-          @click="changeCount('children', -1)"
-        >
-          remove
-        </span>
+        <span class="material-icons guest-count-icon" @click="changeCount('children', -1)"> remove </span>
         <span class="guests-number">{{ guestSelected.children }}</span>
-        <span
-          class="material-icons guest-cout-icon"
-          @click="changeCount('children', 1)"
-        >
-          add
-        </span>
+        <span class="material-icons guest-count-icon" @click="changeCount('children', 1)"> add </span>
       </div>
     </div>
-    <div class="guests-btns-conteiner">
+    <div class="guests-btns-container">
       <!-- <div class="guest-modal-btn" @click="closeGuestsModal">Cancel</div> -->
       <div class="guest-modal-btn" @click="onSelectGuests">OK</div>
     </div>
@@ -58,43 +38,30 @@ export default {
   data() {
     return {
       guestSelected: null,
-    };
+    }
   },
   created() {
-    this.guestSelected = { ...this.guests };
+    this.guestSelected = { ...this.guests }
   },
   methods: {
     closeGuestsModal() {
-      this.guestSelected = { ...this.guests };
-      this.$emit("closeGuestsModal");
+      this.guestSelected = { ...this.guests }
+      this.$emit('closeGuestsModal')
     },
     changeCount(type, val) {
-      if (type === "adults" && this.guestSelected.adults === 0 && val === -1)
-        return;
-      if (
-        type === "children" &&
-        this.guestSelected.children === 0 &&
-        val === -1
-      )
-        return;
+      if (type === 'adults' && this.guestSelected.adults === 0 && val === -1) return
+      if (type === 'children' && this.guestSelected.children === 0 && val === -1) return
 
-      if (type === "children" && val === 1 && this.guestSelected.adults === 0)
-        this.guestSelected.adults = 1;
+      if (type === 'children' && val === 1 && this.guestSelected.adults === 0) this.guestSelected.adults = 1
 
-      if (
-        type === "adults" &&
-        this.guestSelected.adults === 1 &&
-        val === -1 &&
-        this.guestSelected.children > 0
-      )
-        return;
+      if (type === 'adults' && this.guestSelected.adults === 1 && val === -1 && this.guestSelected.children > 0) return
 
-      this.guestSelected[type] += val;
-      this.$emit("onSelectGuests", { ...this.guestSelected });
+      this.guestSelected[type] += val
+      this.$emit('onSelectGuests', { ...this.guestSelected })
     },
     onSelectGuests() {
-      this.$emit("closeGuestsModal");
+      this.$emit('closeGuestsModal')
     },
   },
-};
+}
 </script>
