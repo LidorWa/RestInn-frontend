3402c72ee8f4408a71cd1d05a82bbaa8808f345e
Lidor Waldman <lidorvl@gmail.com>
2022-03-30 09:46:33 +0300

    convert the stay-service to work with http-service

--- src/services/stay-service.js
+++ src/services/stay-service.js
@@ -1,4 +1,4 @@
-// import stays from "../data/stay.json";
+import { httpService } from "../../../../misterToy/frontend/src/services/http.service";
 import { storageService } from "./async-storage-service";
 
 const KEY = "stay_db";
@@ -6,27 +6,46 @@ const KEY = "stay_db";
 export const stayService = {
   query,
   getById,
-  // get,
-  // post,
-  // put,
-  // remove,
-  // postMany,
+  save,
+  remove,
 };
 
 async function query(filterBy = null) {
-  let stays = await storageService.query(KEY);
+  return await httpService.get(`stay`, filterBy)
+  // return httpService.get(`query`)
+  // let stays = await storageService.query(KEY);
 
-  if (!stays || !stays.length) {
-    stays = _createStays();
-  }
+  // if (!stays || !stays.length) {
+  //   stays = _createStays();
+  // }
 
-  return stays;
+  // return stays;
 }
 
 async function getById(stayId) {
-  let stay = await storageService.get(KEY, stayId);
-  return stay;
+
+  return await httpService.get(`stay/getById/${stayId}`)
+  // let stay = await storageService.get(KEY, stayId);
+  // return stay;
 }
+
+async function remove(stayId) {
+  return await httpService.delete(`stay/${stayId}`)
+}
+
+async function save(stay) {
+  if (toy._id) {
+    return await httpService.put(`stay/${stay._id}`, stay)
+  } else {
+    return await httpService.post('stay', stay)
+  }
+}
+
+// function getEmptyStay() {
+//   return {
+
+//   }
+// }
 
 function _createStays() {
   const stays = [
@@ -4503,6 +4522,5 @@ function _createStays() {
   ];
 
   storageService.postMany(KEY, stays);
-  console.log(stays[0]);
   return stays;
 }
