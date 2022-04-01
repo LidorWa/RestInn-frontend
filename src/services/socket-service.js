c154985557b4619cd634999a714b7c317f9abafd
Lidor Waldman <lidorvl@gmail.com>
2022-04-01 23:09:56 +0300

    added frontend socket service

---
+++ src/services/socket-service.js
@@ -0,0 +1,35 @@
+import io from 'socket.io-client'
+
+const baseUrl = (process.env.NODE_ENV === 'production') ? '' : '//localhost:3030'
+export const socketService = createSocketService()
+// export const socketService = createDummySocketService()
+
+// For DEBUG:
+// window.socketService = socketService
+socketService.setup()
+
+
+function createSocketService() {
+  var socket = null;
+  const socketService = {
+    async setup() {
+      socket = io(baseUrl)
+    },
+    on(eventName, cb) {
+      socket.on(eventName, cb)
+      console.log(eventName)
+    },
+    off(eventName, cb=null) {
+      if (!socket) return;
+      if (!cb) socket.removeAllListeners(eventName)
+      else socket.off(eventName, cb)
+    },
+    emit(eventName, data) {
+      socket.emit(eventName, data)
+    },
+    terminate() {
+      socket = null
+    }
+  }
+  return socketService
+}
