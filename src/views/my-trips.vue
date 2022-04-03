1a7c0f079d6da76171b7b39acde8d4e729e69c6d
Liran Parti <liranpar@gmail.com>
2022-04-03 11:18:09 +0300

    fixed bug charts bugs

--- src/views/my-trips.vue
+++ src/views/my-trips.vue
@@ -87,18 +87,22 @@ export default {
       const filterBy = {
         userId: this.loggedInUser?._id,
       };
-      try {
-        await this.$store.dispatch({ type: "loadOrdersWithSocket", filterBy });
-      } catch (err) {
-        console.log("Error while loading orders: ", err);
-      }
 
-      console.log(order.status);
-      const message = {
-        text: `Your order has been ${order.status}`,
-        from: "host",
-      };
-      this.showMessage(message);
+      setTimeout(async () => {
+        try {
+          await this.$store.dispatch({
+            type: "loadOrdersWithSocket",
+            filterBy,
+          });
+          const message = {
+            text: `Your order has been ${order.status}`,
+            from: "host",
+          };
+          this.showMessage(message);
+        } catch (err) {
+          console.log("Error while loading orders: ", err);
+        }
+      }, 2000);
     },
   },
   components: {
