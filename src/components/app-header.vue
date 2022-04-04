6c635e9b8e99325bc5bdfbe098ac778ed01b6b76
Tal Itay <talitay7@gmail.com>
2022-04-04 13:41:51 +0300

    final? header render states and responsiveness

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="main-header-section">
     <header class="main-header-container flex flex-column align-center" :class="{ [headerClass]: true, [layout]: true }">
-      <div class="logo-nav-container flex space-between">
+      <div class="logo-nav-container flex">
         <div class="logo flex align-center" @click="goHome">
           <i class="fa-brands fa-airbnb"></i>
           <h1 class="logo-txt">RestInn</h1>
