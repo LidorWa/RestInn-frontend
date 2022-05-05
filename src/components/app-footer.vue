bc6a80a5bc757bec4d009d41e7a05e653575d0e5
Liran Parti <liranpar@gmail.com>
2022-05-05 19:32:39 +0300

    first commit

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,34 +1,16 @@
 <template>
-<section class="footer flex justify-center aline-center ">
-  <!-- <div class="rights-nav-container flex"> -->
+  <section class="footer flex justify-center aline-center">
+    <!-- <div class="rights-nav-container flex"> -->
     <div class="rights">
       <p>&#169; 2022 RestInn</p>
     </div>
-
-    <!-- <div class="footer-nav">
-     <ul>
-        <li>Privacy </li>
-        <li>Terms </li>
-        <li>Sitemap </li>
-      </ul>
-    </div>
-  </div> -->
-
-<!-- <div class="social-media-container flex align-center space-between">
-<i class="fa-brands fa-facebook-f"></i>
-<i class="fa-brands fa-instagram"></i>
-<i class="fa-brands fa-twitter"></i> -->
-
-<!-- </div> -->
-
   </section>
-
 </template>
 
 <script>
 export default {
-  name: "app-footer"
-}
+  name: "app-footer",
+};
 </script>
 
 <style></style>
