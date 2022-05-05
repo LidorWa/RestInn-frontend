c8905bd532977bb3f9efee966d995097b6b5e498
Liran Parti <liranpar@gmail.com>
2022-05-05 19:49:02 +0300

    removed footer comments

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -4,6 +4,22 @@
     <div class="rights">
       <p>&#169; 2022 RestInn</p>
     </div>
+
+    <!-- <div class="footer-nav">
+     <ul>
+        <li>Privacy </li>
+        <li>Terms </li>
+        <li>Sitemap </li>
+      </ul>
+    </div>
+  </div> -->
+
+    <!-- <div class="social-media-container flex align-center space-between">
+<i class="fa-brands fa-facebook-f"></i>
+<i class="fa-brands fa-instagram"></i>
+<i class="fa-brands fa-twitter"></i> -->
+
+    <!-- </div> -->
   </section>
 </template>
 
